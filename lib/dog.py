4f1facf348e4b075c37ef3de8a3259de6106f050
heebatullah <heebahassan182@gmail.com>
2025-12-04 18:06:42 +0300

    Complete Attributes and Properties lab with Dog and Person classes

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,33 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", 
+                       "French Bulldog", "Pug", "Pointer"]
+
+    def __init__(self, name="Unknown", breed="Beagle"):
+        self.name = name  # uses setter
+        self.breed = breed  # uses setter
+
+    # NAME PROPERTY
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    # BREED PROPERTY
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in self.approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
